3d1be46dc9a4b66b709bc28ed03c3debe25f489b
Viktor <levik04122008@yandex.ru>
2016-02-09 16:05:45 +0300

    last commit

--- tests/dz3.php
+++ tests/dz3.php
@@ -14,5 +14,12 @@ require __DIR__ . '\..\autoload.php';
 //$news= App\Models\News::findById(3);
 //echo $news->author_id;
 //$news->author;
-$author = App\Models\Author::findById(1);
-var_dump($author);
+//$author = App\Models\Author::findById(1);
+//var_dump($author);
+
+//Задание 3 Использовано в App\templates\listNews.php
+
+//Задание 4 Использовано в App\templates\listController.php
+//$view = new \App\View();
+//$view['news']= App\Models\News::findAll();
+//$view['title'] = 'Список новостей';
